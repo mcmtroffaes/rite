7914f6c88b85730b14e0f2315376f7d6cad902bc
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-01-17 14:55:37 +0000

    Runtime check for protocol is very limited, so disable it.

--- rite/backend/__init__.py
+++ rite/backend/__init__.py
@@ -5,7 +5,6 @@ from rite.richtext import BaseText
 RenderType = TypeVar('RenderType', covariant=True)
 
 
-@runtime_checkable
 class RenderProtocol(Protocol[RenderType]):
     def __call__(self, text: BaseText) -> Iterable[RenderType]:
         pass
