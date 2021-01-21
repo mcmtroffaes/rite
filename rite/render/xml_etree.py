9eeb131a34589f3d3a538b8a9ff51aff9cc9a132
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-01-21 16:47:52 +0000

    Simplify implementation of xml_etree renderer.

--- rite/render/xml_etree.py
+++ rite/render/xml_etree.py
@@ -15,33 +15,31 @@ def escape(value: str) -> str:
 # generates a string sequence followed by one or more xml elements
 # strings in between elements are in the "tail" attribute of each element
 @singledispatch
-def render_xml_etree(text: BaseText
-                     ) -> Tuple[Optional[str], Iterable[Element]]:
+def render_xml_etree(text: BaseText) -> Tuple[str, Iterable[Element]]:
     return ''.join(map(escape, text_iter(text))), []
 
 
 @render_xml_etree.register(Tag)
-def _tag(text: Tag) -> Tuple[Optional[str], Iterable[Element]]:
+def _tag(text: Tag) -> Tuple[str, Iterable[Element]]:
     element = Element(text.tag.value)
     element.text, children = render_xml_etree(text.text)
     element.extend(children)
-    return None, [element]
+    return '', [element]
 
 
 @render_xml_etree.register(Join)
-def _join(text: Join) -> Tuple[Optional[str], Iterable[Element]]:
+def _join(text: Join) -> Tuple[str, Iterable[Element]]:
     head_text: str = ''
     children: List[Element] = []
     for part in text.parts:
         part_text, part_children = render_xml_etree(part)
-        if part_text:
-            if children:
-                last_element = children[-1]
-                if last_element.tail is None:
-                    last_element.tail = part_text
-                else:
-                    last_element.tail += part_text
+        if children:
+            last_element = children[-1]
+            if last_element.tail is None:
+                last_element.tail = part_text
             else:
-                head_text += part_text
+                last_element.tail += part_text
+        else:
+            head_text += part_text
         children.extend(part_children)
     return head_text, children
